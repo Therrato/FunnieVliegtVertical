8f3141fc82a80e7b1cb51fff6c58c6934d546ef8
Jeroen van Dragt <jeroen.van.dragt@gmail.com>
2013-09-16 15:35:25 +0200

    Kinect rotation added

--- VSlice/Assets/Scripts/FunnieMovementScript.cs
+++ VSlice/Assets/Scripts/FunnieMovementScript.cs
@@ -9,16 +9,24 @@ public class FunnieMovementScript : MonoBehaviour {
 	public GameObject handLeft;
 	public GameObject handRight;
 	
+	
 	// Use this for initialization
 	void Start () 
 	{
-		
+		Debug.Log((handRight.transform.position.x - handLeft.transform.position.x));
 		
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		this.gameObject.transform.rotation = new Quaternion(Mathf.Atan((handRight.transform.position.y - handLeft.transform.position.y)/(handRight.transform.position.x-handLeft.transform.position.x)),0f,0f,0f);
+		if((handRight.transform.position.y - handLeft.transform.position.y) != 0)
+		{	
+			
+			this.gameObject.transform.eulerAngles = new Vector3(0,0,Mathf.Atan((handRight.transform.position.y - handLeft.transform.position.y)/(handRight.transform.position.x-handLeft.transform.position.x))*Mathf.Rad2Deg*-1);
+			Debug.Log ("angle of arms "+Mathf.Atan((handRight.transform.position.y - handLeft.transform.position.y)/(handRight.transform.position.x-handLeft.transform.position.x))* Mathf.Rad2Deg);
+			
+		}
+		else return;
 	}
 }
