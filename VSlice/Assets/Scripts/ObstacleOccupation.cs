e8fc74fa23a4baed792ef9aa085a39b61b18c01c
Roy Schroder <r.schroder@live.nl>
2013-09-17 11:35:29 +0200

    ObstaccleOccupation

---
+++ VSlice/Assets/Scripts/ObstacleOccupation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class ObstacleOccupation
+{
+    private int[] occupates = new int[3];
+
+	public ObstacleOccupation(int xSpots, int hSpots, int zSpots)
+	{
+        occupates[0] = xSpots;
+        occupates[1] = hSpots;
+        occupates[2] = zSpots;
+
+
+	}
+
+    public int[] getOccupation()
+    {
+        return occupates;
+    }
+
+
+}
